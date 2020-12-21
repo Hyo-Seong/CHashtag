ff2b7bbf7b5bd43117d64839694663227ac42c81
Hyo-Seong <gytjd090792@gmail.com>
2020-12-21 23:20:06 +0900

    csv 데이터를 저장할 User 모델(객체)를 만듦

---
+++ ParseCSV/ParseCSV/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseCSV.Models
+{
+    public class User : BindableBase
+    {
+        private int _index;
+        public int Index
+        {
+            get => _index;
+            set => SetProperty(ref _index, value);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        private string _location;
+        public string Location
+        {
+            get => _location;
+            set => SetProperty(ref _location, value);
+        }
+
+        private string _job;
+        public string Job
+        {
+            get => _job;
+            set => SetProperty(ref _job, value);
+        }
+    }
+}
