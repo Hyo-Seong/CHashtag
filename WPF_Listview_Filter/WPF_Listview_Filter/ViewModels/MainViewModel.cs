350f2870ac5053e741c40e9f612c16281f24741a
Hyo-Seong <gytjd090792@gmail.com>
2021-02-09 01:12:09 +0900

    사용하지 않는 using을 제거함

--- WPF_Listview_Filter/WPF_Listview_Filter/ViewModels/MainViewModel.cs
+++ WPF_Listview_Filter/WPF_Listview_Filter/ViewModels/MainViewModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using WPF_Listview_Filter.Models;
 
