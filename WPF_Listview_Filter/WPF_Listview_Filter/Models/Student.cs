6f9306820223de47285c8ce282db081f261fa50c
Hyo-Seong <gytjd090792@gmail.com>
2021-02-09 00:07:40 +0900

    Add Student Model

---
+++ WPF_Listview_Filter/WPF_Listview_Filter/Models/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Listview_Filter.Models
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private int _age;
+        private int Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        private string _country;
+        public  string Country
+        {
+            get => _country;
+            set
+            {
+                _country = value;
+                OnPropertyChanged(nameof(Country));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
