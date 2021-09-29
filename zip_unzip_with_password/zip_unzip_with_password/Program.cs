0afa06c094f17190475132ccfd386110acff0894
Hyo-Seong <gytjd090792@gmail.com>
2021-09-29 21:22:48 +0900

    zip_unzip_with_password 프로젝트 생성

---
+++ zip_unzip_with_password/zip_unzip_with_password/Program.cs
@@ -0,0 +1,43 @@
+﻿using Ionic.Zip;
+
+namespace zip_unzip_with_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipPath = @"C:\Users\hyoseong\Desktop\test.zip";
+            string zipPath2 = @"C:\Users\hyoseong\Desktop\test2.zip";
+            string path = @"C:\Users\hyoseong\Desktop\123.txt";
+            string unzipPath = @"C:\Users\hyoseong\Desktop\aa";
+            string unzipPath2 = @"C:\Users\hyoseong\Desktop\aa2";
+            string password = "test123";
+
+            new Program().Zip(path, zipPath, password);
+            new Program().Zip(path, zipPath2);
+
+            new Program().UnZip(zipPath, unzipPath, password);
+            new Program().UnZip(zipPath2, unzipPath2);
+
+        }
+
+        public void Zip(string directoryPath, string zipPath, string password = null)
+        {
+            using (ZipFile zip = new ZipFile(zipPath))
+            {
+                zip.Password = password;
+                zip.AddFile(directoryPath, string.Empty);
+                zip.Save(zipPath);
+            }
+        }
+
+        public void UnZip(string zipFilePath, string unZipPath, string password = null)
+        {
+            using (ZipFile zip = new ZipFile(zipFilePath))
+            {
+                zip.Password = password;
+                zip.ExtractAll(unZipPath);
+            }
+        }
+    }
+}
